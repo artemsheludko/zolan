e676285a374b0659f0fa25d7a21259fe36097f17
artemsheludko <sheludko.artem@gmail.com>
2019-08-14 14:59:46 +0300

    fixed search issue

--- js/common.js
+++ js/common.js
@@ -51,18 +51,6 @@ $(document).ready(function() {
     searchBox.removeClass("is-visible");
   }
 
-
-  /* =======================
-  // Simple Jekyll Search
-  ======================= */
-  SimpleJekyllSearch({
-    searchInput: document.getElementById("js-search-input"),
-    resultsContainer: document.getElementById("js-results-container"),
-    json: "/search.json",
-    searchResultTemplate: '<li class="search-results__item"><a class="search-results__link" href="{url}">{title}</a><br> <span class="search-results__date">{date}</span></li>',
-    noResultsText: '<li class="no-results">No results found</li>'
-  });
-
   /* =======================
   // Responsive Videos
   ======================= */
